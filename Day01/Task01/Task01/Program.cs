3f337bc915aa36eaa3684daa8d62ea92bff4a541
ninab <ninabeuklishvili00@gmail.com>
2023-10-09 00:13:59 +0400

    finished task03/day02

--- Day01/Task01/Task01/Program.cs
+++ Day01/Task01/Task01/Program.cs
@@ -1,39 +1,91 @@
 ﻿
-Random random = new Random();
-int randomNumber = random.Next(1, 11);
 
-int maxAttempts = 3;
-
-Console.WriteLine("Welcome to the Number Guessing Game!");
-Console.WriteLine("You have " + maxAttempts + " attempts to guess the number.");
-
-for (int attempts = 1; attempts <= maxAttempts; attempts++)
+Console.Write("Enter the size of the matrix (2 or 3): ");
+if (int.TryParse(Console.ReadLine(), out int size) && (size == 2 || size == 3))
 {
-    Console.Write("Attempt " + attempts + ": Enter a number between 1 and 10(included): ");
+    int[,] matrix = new int[size, size];
 
-    if (int.TryParse(Console.ReadLine(), out int userNumber))
+    Console.WriteLine("Enter the matrix elements row by row:");
+
+    for (int row = 0; row < size; row++)
     {
-        if (userNumber >= 1 && userNumber <= 10)
+        for (int col = 0; col < size; col++)
         {
-            if (userNumber == randomNumber)
+            Console.Write($"Enter element at row {row + 1}, column {col + 1}: ");
+            if (int.TryParse(Console.ReadLine(), out int element))
             {
-                Console.WriteLine("Congratulations! You guessed the correct number!");
-                return;
+                matrix[row, col] = element;
             }
             else
             {
-                Console.WriteLine("Sorry, that's not the correct number.");
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                col--;
             }
         }
-        else
-        {
-            Console.WriteLine("Please enter a number between 1 and 10.");
-        }
+    }
+
+    // Calculate determinant
+    int determinant = CalculateDeterminant(matrix);
+
+    // Print the matrix
+    Console.WriteLine("\nEntered matrix:");
+    PrintMatrix(matrix);
+
+    // Print the determinant
+    Console.WriteLine("\nDeterminant of the matrix: " + determinant);
+}
+else
+{
+    Console.WriteLine("Invalid input for matrix size. Please enter 2 or 3.");
+}
+
+
+// Calculate determinant of a 2x2 or 3x3 matrix
+static int CalculateDeterminant(int[,] matrix)
+{
+    if (matrix.GetLength(0) == 2)
+    {
+        // For a 2x2 matrix: ad - bc
+        int a = matrix[0, 0];
+        int b = matrix[0, 1];
+        int c = matrix[1, 0];
+        int d = matrix[1, 1];
+        return (a * d) - (b * c);
+    }
+    else if (matrix.GetLength(0) == 3)
+    {
+        // For a 3x3 matrix using the formula
+        int a = matrix[0, 0];
+        int b = matrix[0, 1];
+        int c = matrix[0, 2];
+        int d = matrix[1, 0];
+        int e = matrix[1, 1];
+        int f = matrix[1, 2];
+        int g = matrix[2, 0];
+        int h = matrix[2, 1];
+        int i = matrix[2, 2];
+        return (a * e * i) + (b * f * g) + (c * d * h) - (c * e * g) - (a * f * h) - (b * d * i);
     }
     else
     {
-        Console.WriteLine("Invalid input. Please enter a valid number.");
+        throw new ArgumentException("Invalid matrix size for determinant calculation.");
     }
 }
 
-Console.WriteLine("Sorry, you didn't guess the number.");
+// Print the matrix
+static void PrintMatrix(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int cols = matrix.GetLength(1);
+
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            Console.Write(matrix[row, col] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
