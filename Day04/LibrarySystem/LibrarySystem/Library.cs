40d871d60b2e5b5a3458944e55f810287639ee23
ninab <ninabeuklishvili00@gmail.com>
2023-10-18 00:40:44 +0400

    finished task01/day04

---
+++ Day04/LibrarySystem/LibrarySystem/Library.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem
+{
+    internal class Library
+    {
+        private Book[] books;
+
+        public Library(int capacity) 
+        {
+            books = new Book[capacity];
+        }
+
+        public void PrintBooks()
+        {
+            Console.WriteLine("\nBooks in the library: ");
+            foreach (Book book in books)
+            {
+                if (book != null)
+                {
+                    Console.WriteLine($"{book.Title} : {book.Author}");
+                }
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                int count = 0;
+                foreach (Book book in books)
+                {
+                    if (book != null)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+
+        public void AddBook(Book book)
+        {
+            for (int i = 0; i < books.Length; i++)
+            {
+                if (books[i] == null)
+                {
+                    books[i] = book;
+                    break;
+                }
+            }
+        }
+
+        public void RemoveBook(Book book)
+        {
+            for (int i = 0; i < books.Length; i++)
+            {
+                if (books[i] != null && books[i].Equals(book))
+                {
+                    books[i] = null;
+                    break;
+                }
+            }
+        }
+
+        public List<Book> FindBooksByTitle(string title)
+        {
+            List<Book> matchingBooks = new List<Book>();
+            foreach (Book book in books)
+            {
+                if (book != null && book.Title.Equals(title, StringComparison.OrdinalIgnoreCase))
+                {
+                    matchingBooks.Add(book);
+                }
+
+            }
+            return matchingBooks;
+        }
+    }
+}
---
+++ Day04/LibrarySystem/LibrarySystem/Program.cs
@@ -0,0 +1,48 @@
+﻿using LibrarySystem;
+
+Library myLibrary = new Library(10);
+
+Book book1 = new Book("The Metamorphosis", "Franz Kafka", 1915);
+Book book2 = new Book("Mysteries", "Knut Hamsun", 1892);
+Book book3 = new Book("1984", "George Orwell", 1949);
+Book book4 = new Book("The Rebel", "Albert Camus", 1951);
+Book book5 = new Book("Dark Matter", "Blake Crouch", 2016);
+Book book6 = new Book("The Metamorphosis", "Franz Kafka2", 1915);
+
+myLibrary.AddBook(book1);
+myLibrary.AddBook(book2);
+myLibrary.AddBook(book3);
+myLibrary.AddBook(book4);
+myLibrary.AddBook(book5);
+myLibrary.AddBook(book6);
+
+myLibrary.PrintBooks();
+
+//Get the number of books
+int numberOfBooks = myLibrary.Count;
+Console.WriteLine($"\nNumber of books in the library: {numberOfBooks} \n");
+
+//Find books by title
+string searchTitle = "The Metamorphosis";
+List<Book> matchingBooks = myLibrary.FindBooksByTitle(searchTitle);
+
+//Print the matching books
+
+if (matchingBooks.Count > 0)
+{
+    Console.WriteLine($"Matching books with the title \"{searchTitle}\":");
+    foreach (Book book in matchingBooks)
+    {
+        Console.WriteLine($"{book.Title} by {book.Author} ({book.Year})");
+    }
+}
+else
+{
+    Console.WriteLine($"No books with the title '{searchTitle}' found.");
+}
+
+//Remove the book
+myLibrary.RemoveBook(book6);
+
+//After removal
+myLibrary.PrintBooks();
